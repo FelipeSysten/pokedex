b68f6a8d2fea6d3518189737938dbe9560d48878
Felipe de Souza <systen3@gmail.com>
2021-11-23 03:34:20 -0300

    logo pokedex

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,15 +1,29 @@
 <template>
-  <hello-world />
+  <div>
+    <div class="center">
+    <img src="https://archives.bulbagarden.net/media/upload/4/4b/Pok%C3%A9dex_logo.png">
+   </div>
+   <div class="content">
+          testee
+      </div>
+    </div>
+
 </template>
 
 <script>
-  import HelloWorld from '../components/HelloWorld'
 
   export default {
-    name: 'Home',
-
-    components: {
-      HelloWorld,
-    },
+   data(){
+     return {
+   
+   }
+  }
   }
 </script>
+
+<style scoped>
+.center{
+  text-align: center;
+  margin-top: 1%;
+}
+</style>
